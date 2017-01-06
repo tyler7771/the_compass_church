5965c268571ce46d3ec0a1765034ccbed803e747
Tyler Fields <tyler.fields777@gmail.com>
2017-01-06 13:22:23 -0800

    campus details

--- frontend/components/visit/visit.jsx
+++ frontend/components/visit/visit.jsx
@@ -32,7 +32,16 @@ class Visit extends React.Component {
             center={"selma"}
           />
           <div className="campus-info-details">
-            selma
+            <div className="campus-info-details-text">
+              <h3>Selma</h3>
+              <p><span className="fa fa-clock-o"></span> Sundays at 9:00 & 10:45am</p>
+              <p><span className="fa fa-phone-square"></span> (765) 282-1648</p>
+              <p><span className="fa fa-envelope"></span> selma@thecompass.church</p>
+              <div className="address">
+                <span><p><span className="fa fa-map-marker"></span> 400 N CR 600 E</p><p>&ensp; SELMA, IN 47383</p></span>
+              </div>
+            </div>
+            <a href="https://www.google.com/maps/dir//The+Compass+Church+-+Selma,+400+N+Co+Rd+600+E,+Selma,+IN+47383/@40.195659,-85.275648,17z/data=!4m15!1m6!3m5!1s0x88153ff7cab93139:0x810f52ff63f7ba3a!2sThe+Compass+Church+-+Selma!8m2!3d40.195659!4d-85.273454!4m7!1m0!1m5!1m1!1s0x88153ff7cab93139:0x810f52ff63f7ba3a!2m2!1d-85.273454!2d40.195659" target="_blank" className="directions"><span className="fa fa-road"></span> Directions</a>
           </div>
         </div>
       );
@@ -42,6 +51,19 @@ class Visit extends React.Component {
           <CampusMap className="campus-map"
             center={"winchester"}
           />
+          <div className="campus-info-details">
+            <div className="campus-info-details-text-winchester">
+              <h3>Winchester</h3>
+              <p><span className="fa fa-clock-o"></span> Sundays at 9:30am</p>
+              <p><span className="fa fa-phone-square"></span> (765) 584-4151</p>
+              <p><span className="fa fa-envelope"></span> winchester@thecompass.church</p>
+              <div className="address">
+                <span><p><span className="fa fa-map-marker"></span> Sunday Location:</p><p>&ensp; TOWNE SQUARE COMMUNITY CENTRE</p><p>&ensp; 123 W FRANKLIN ST</p><p>&ensp; WINCHESTER, IN 47394</p></span>
+                <span><p><span className="fa fa-map-marker"></span> Office Location:</p><p>&ensp; 313 S MERIDIAN ST</p><p>&ensp; WINCHESTER, IN 47394</p></span>
+              </div>
+            </div>
+            <a href="https://www.google.com/maps/dir//123+W+Franklin+St,+Winchester,+IN+47394/@40.171474,-84.9843989,17z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x88155e4d3105997b:0x9e4d4f01fafb981a!2m2!1d-84.9822049!2d40.171474" target="_blank" className="directions"><span className="fa fa-road"></span> Directions</a>
+          </div>
         </div>
       );
     } else if (this.state.locationDetails === "yorktown") {
@@ -50,6 +72,18 @@ class Visit extends React.Component {
           <CampusMap className="campus-map"
             center={"yorktown"}
           />
+          <div className="campus-info-details">
+            <div className="campus-info-details-text">
+              <h3>Yorktown</h3>
+              <p><span className="fa fa-clock-o"></span> Sundays at 10:45am</p>
+              <p><span className="fa fa-phone-square"></span> (765) 405-1119</p>
+              <p><span className="fa fa-envelope"></span> yorktown@thecompass.church</p>
+              <div className="address">
+                <span><p><span className="fa fa-map-marker"></span> 8116 W JACKSON ST</p><p>&ensp; MUNCIE, IN 47304</p></span>
+              </div>
+            </div>
+            <a href="https://www.google.com/maps/dir//The+Compass+Church+-+Yorktown,+8116+W+Jackson+St,+Muncie,+IN+47304/@40.2108617,-85.5166683,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x8815227cd4542c5b:0xfaddb1cb57dcc2de!2m2!1d-85.481563!2d40.210867" target="_blank" className="directions"><span className="fa fa-road"></span> Directions</a>
+          </div>
         </div>
       );
     }
