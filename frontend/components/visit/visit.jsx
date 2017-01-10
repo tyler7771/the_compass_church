99c5b6dcd93d8de85c2f608184ab5bf8e2fcbf36
Tyler Fields <tyler.fields777@gmail.com>
2017-01-09 23:16:51 -0800

    add what to expect section

--- frontend/components/visit/visit.jsx
+++ frontend/components/visit/visit.jsx
@@ -121,6 +121,21 @@ class Visit extends React.Component {
         </div>
         <div className="what-to-expect">
           <h2>WHAT TO EXPECT</h2>
+          <div className="what-to-expect-content">
+            <div className="what-to-expect-image"></div>
+            <div className="what-to-expect-text">
+              <p>We believe that God has the power to change lives – and we have seen Him do just that. Marriages are being restored, addictions overcome, and people are taking steps toward reaching their full potential in Christ. Our desire is that here at The Compass Church, you’ll experience these things for yourself.</p>
+              <br/>
+              <p>What can you expect at The Compass Church:</p>
+              <br/>
+              <ul>
+                <li>&ensp;• A warm welcom from friendly faces</li>
+                <li>&ensp;• Environments for every age</li>
+                <li>&ensp;• Engaging worship and a Relevant message</li>
+              </ul>
+          </div>
+          </div>
+
         </div>
       </div>
     );
