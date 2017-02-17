82e900ed9c9b6d8eeca11d140ea3acf652a7982a
Tyler Fields <tyler.fields777@gmail.com>
2017-02-16 23:24:09 -0800

    update

--- frontend/components/header/home/header.jsx
+++ frontend/components/header/home/header.jsx
@@ -93,41 +93,41 @@ class Header extends React.Component {
               <div className="header-about">
                 <Link to="/about">ABOUT</Link>
                   <ul className="about-dropdown">
-                    <a href="/#/staff"><li>STAFF</li></a>
-                    <a href="/#/mission"><li>MISSION</li></a>
+                    <Link to="/staff"><li>STAFF</li></Link>
+                    <Link to="/mission"><li>MISSION</li></Link>
                   </ul>
               </div>
               <div className="header-connect">
                 <Link to="/connect">CONNECT</Link>
                   <ul className="connect-dropdown">
-                    <a href="/children"><li>CHILDREN</li></a>
-                    <a href="/youth"><li>YOUTH</li></a>
-                    <a href="/camps"><li>CAMPS</li></a>
-                    <a href="/young-adults"><li>YOUNG ADULTS</li></a>
-                    <a href="/life-groups"><li>LIFE GROUPS</li></a>
-                    <a href="/missions"><li>MISSIONS</li></a>
-                    <a href="/care"><li>CARE</li></a>
+                    <Link to="/children"><li>CHILDREN</li></Link>
+                    <Link to="/youth"><li>YOUTH</li></Link>
+                    <Link to="/camps"><li>CAMPS</li></Link>
+                    <Link to="/young-adults"><li>YOUNG ADULTS</li></Link>
+                    <Link to="/life-groups"><li>LIFE GROUPS</li></Link>
+                    <Link to="/missions"><li>MISSIONS</li></Link>
+                    <Link to="/care"><li>CARE</li></Link>
                   </ul>
               </div>
               <div className="header-next-step">
                 <Link to="/next-steps">NEXT STEPS</Link>
                   <ul className="next-step-dropdown">
-                    <a href="/volunteer"><li>VOLUNTEER</li></a>
-                    <a href="/baptism"><li>BAPTISM</li></a>
-                    <a href="/membership"><li>MEMBERSHIP</li></a>
-                    <a href="/salt"><li>LEADERSHIP TRAINING (SALT)</li></a>
+                    <Link to="/volunteer"><li>VOLUNTEER</li></Link>
+                    <Link to="/baptism"><li>BAPTISM</li></Link>
+                    <Link to="/membership"><li>MEMBERSHIP</li></Link>
+                    <Link to="/salt"><li>LEADERSHIP TRAINING (SALT)</li></Link>
                   </ul>
               </div>
               <div className="header-daycare">
                 <Link to="/childcare">CHILD CARE</Link>
                   <ul className="daycare-dropdown">
-                    <a href="/little-learners"><li>LITTLE LEARNERS</li></a>
-                    <a href="/little-scholars"><li>LITTLE SCHOLARS</li></a>
+                    <Link to="/little-learners"><li>LITTLE LEARNERS</li></Link>
+                    <Link to="/little-scholars"><li>LITTLE SCHOLARS</li></Link>
                   </ul>
               </div>
               <a>SERMONS</a>
               <a>CONTACT</a>
-              <a href="https://thecompasschurch.ccbchurch.com/trx_give.php" className="give">Give</a>
+              <a href="https://thecompasschurch.ccbchurch.com/trx_give.php" className="give">GIVE</a>
             </div>
           </div>
         </header>
